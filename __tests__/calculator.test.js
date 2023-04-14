34c4cb60790d2ebee8883f9374b23027680dc513
Max Betich <betichm@gmail.com>
2023-04-14 08:34:26 -0700

    begin testing ageConverter method

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -6,9 +6,14 @@ describe('Calculator', () => {
 
   beforeEach(() => {
     newCalculator = new Calculator(30);
+    newCalculator.ageConverter();
   });
 
   test('should successfully create a new Calculator object with values for Earth, Mercury, Venus, Mars, and Jupiter ages', () => {
     expect(newCalculator.earthAge).toEqual(30);
+  });
+
+  test('should successfully convert earthAge to mercuryAge', () => {
+    expect(newCalculator.mercuryAge).toEqual(125);
   })
 })
