777458fed0db138087f5f7efc986e8b251c2fc55
Max Betich <betichm@gmail.com>
2023-04-14 08:30:02 -0700

    add Calculator constructor

---
+++ src/calculator.js
@@ -0,0 +1,9 @@
+export default class Calculator {
+  constructor(age) {
+    this.earthAge = age;
+    this.mercuryAge = 0;
+    this.venusAge = 0;
+    this.marsAge = 0;
+    this.jupiterAge = 0;
+  }
+}
