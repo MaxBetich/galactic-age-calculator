628cc8199c8c9ce3cfa8dc48ec7fef1b65229fdf
Max Betich <betichm@gmail.com>
2023-04-14 09:50:17 -0700

    expand pastBirthday method functionality

--- src/calculator.js
+++ src/calculator.js
@@ -6,7 +6,8 @@ export default class Calculator {
     this.marsAge = 0;
     this.jupiterAge = 0;
     this.passedEarthYears = 0;
-    this.passedMercuryYears = 0;  
+    this.passedMercuryYears = 0;
+    this.passedVenusYears = 0;
   }
   
   ageConverter() {
@@ -22,5 +23,6 @@ export default class Calculator {
     const pastAge = birthday;
     this.passedEarthYears = parseFloat((age - pastAge).toFixed(2));
     this.passedMercuryYears = parseFloat(((age - pastAge) / 0.24).toFixed(2));
+    this.passedVenusYears = parseFloat(((age - pastAge) / 0.62).toFixed(2));
   }
 }
