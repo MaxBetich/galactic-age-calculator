214f7d45e094a1ea518cb1c93d4123c24f542cce
Max Betich <betichm@gmail.com>
2023-04-14 08:55:04 -0700

    expand functionality of ageConverter method

--- src/calculator.js
+++ src/calculator.js
@@ -11,5 +11,6 @@ export default class Calculator {
     let age = this.earthAge;
     this.mercuryAge = Math.floor(age / 0.24);
     this.venusAge = Math.floor(age / 0.62);
+    this.marsAge = Math.floor(age / 1.88);
   };
 }
